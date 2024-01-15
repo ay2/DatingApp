ae70ed8fe3cb8f54ef8537b2548673fcb373d42b
Andrey <andrey.yakubov@ericsson.com>
2024-01-15 20:21:14 +0200

    Authentication basics

--- API/Program.cs
+++ API/Program.cs
@@ -1,19 +1,20 @@
 
+using System.Text;
 using API.Data;
+using API.Interfaces;
+using API.Services;
+using API.Extensions;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-builder.Services.AddDbContext<DataContext>(opt =>
-{
-    var connstr = builder.Configuration.GetConnectionString("DefaultConnection");
-    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
-    //opt.UseSqlite("Data Source=datingapp.db");
-});
-builder.Services.AddCors();
+builder.Services.AddApplicationServices(builder.Configuration);
+builder.Services.AddIdentityServices(builder.Configuration);
 
 var app = builder.Build();
 
@@ -21,7 +22,7 @@ var app = builder.Build();
 app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 
 app.UseHttpsRedirection();
-
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
